cc04bcd910594985f5a33f2a1e7b72daa26d0e86
Monday_David_Sarka <david_md@hamari.agency>
2023-05-10 12:46:17 +0100

    Fixed paddings and static positions

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="text-white font-bold">
         <div className="flex container mx-auto px-8 justify-between items-center">
-            <Link to="/" className="text-4xl font-serif hidden md:block">Logo</Link>
+            <Link to="/" className="text-2xl md:text-4xl font-serif absolute md:static left-4 top-8 md:block">Logo</Link>
             <div className="flex md:flex-row items-center justify-center flex-col w-full md:w-auto p-8 gap-2 md:gap-8">
                 <Link to="add-book">Add Books</Link>
                 <Link to="view-books">View Books</Link>
