9f599be9b4675a3e974cdb27478007850d82e9a3
Monday_David_Sarka <david_md@hamari.agency>
2023-05-11 00:58:35 +0100

    Mopc

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -15,10 +15,10 @@ const AddBook = ({ title, setTitle, todos, setTodos}) => {
   };
 
   return (
-    <section>
-      <div className="container mx-auto">
+    <section className="bg-white md:bg-transparent">
+      <div className="mx-auto container">
         <div className="form-header">
-          <form className="flex flex-col mx-auto pt-5 mb-12 pb-8 px-2 md:px-12 bg-white w-full md:w-[90%] max-w-[600px] md:mt-12" onSubmit={onFormSubmit}>
+          <form className="flex flex-col mx-auto pt-5 mb-12 pb-8 px-2 md:px-12 md:bg-white  w-full md:w-[90%] max-w-[600px] md:mt-12" onSubmit={onFormSubmit}>
             <h3 className="form-header">Add a Book to your Library</h3>
             <input
               type="text"
